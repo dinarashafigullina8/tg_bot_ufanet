57319c228c930b6614d7d69598544f51d18be3d1
Dinara <dineirashafigulllina@gmail.com>
2022-11-15 13:34:58 +0500

    made change in db and main

--- db.py
+++ db.py
@@ -39,7 +39,7 @@ class BotDB:
 
     def get_user_id(self, telegramUserId):
         """Достаем юзера в базе по его user_id"""
-        cursor.execute("SELECT 'telegramUserId' FROM telegram_bot.user WHERE 'telegramUserId' = %s", (telegramUserId,))
+        cursor.execute("SELECT * FROM telegram_bot.user WHERE telegramUserId = %s", (telegramUserId,))
         return cursor.fetchone()
 
     def create_user(self, telegramUserId):
@@ -50,47 +50,65 @@ class BotDB:
     def create_message(self, user_id, text):
         """Создаем запись сообщения"""
         cursor.execute("INSERT INTO telegram_bot.message (user_id,text) VALUES (%s, %s)",(user_id, text))
-        return cnx.commit()
-    
-    def read_last(self, text):
+        last_id = cursor.lastrowid
+        return cnx.commit(), last_id 
+
+    def read_last(self, user_id):
         """Выводим последнее сообщение"""
-        result = self.cursor.execute("SELECT LAST_VALUE ('text') OVER (ORDER BY 'createdAT') FROM 'message' ", (text,))
-        return result.fetchone()[0]
+        cursor.execute("SELECT text FROM telegram_bot.message WHERE user_id = %s ORDER BY createdAT DESC LIMIT 1", (user_id,))
+        return cursor.fetchone()
 
-    def read_id(self, message_id):
+    def read_id(self, message_id, user_id):
         """Выводим поле сообщение с указанным id"""
-        result = self.cursor.execute("SELECT 'text' FROM 'messsage' WHERE 'message_id' = ?", (message_id))
-        return result.fetchone()[0]
+        cursor.execute("SELECT text FROM telegram_bot.message WHERE message_id = %s AND user_id = %s", (message_id, user_id))
+        return cursor.fetchone()
     
-    def read_all(self,text):
-        """Выводим все сообщения пользователя"""
-        result = self.cursor.execute("SELECT 'text' FROM 'messsage' ORDER BY 'createdAT'", (text,))
-        return result.fetchone()[0]
+    def message_id_exist(self, message_id, user_id):
+        """Проверяем есть ли сообщение в базе"""
+        cursor.execute("SELECT message_id FROM telegram_bot.message WHERE message_id = %s AND user_id = %s", (message_id, user_id))
+        return bool(len(cursor.fetchall())), user_id
 
-    def read_tag(self,text):
+    def read_all(self, user_id):
+        """Выводим все сообщения пользователя"""
+        cursor.execute("SELECT text FROM telegram_bot.message WHERE user_id = %s ORDER BY createdAT", (user_id,))
+        return cursor.fetchall()
+
+    def read_tag(self,user_id, text):
         """Выводим все сообщения с тэгом"""
-        result = self.cursor.execute("SELECT 'text' FROM 'message' INNER JOIN 'message_tag' WHERE 'message.message_id' = 'message_tag.message_id'INNER JOIN 'tag' WHERE 'message_tag.tag_id' = 'tag.tag_id' and 'tag' = ?", (text,))
-        return result.fetchone()[0]
+        cursor.execute("SELECT text FROM telegram_bot.message WHERE text LIKE %s AND user_id = %s", ('%%' + text + '%%', user_id))
+        return cursor.fetchall()
         
+    def tag_exist(self, name):
+        """Проверяем есть ли тэг в базе"""
+        cursor.execute("SELECT name FROM telegram_bot.tag WHERE name = %s", (name,))
+        return bool(len(cursor.fetchall()))
+    
     def write_tag_new(self, name, descript):
         """Записываем новый тэг"""
-        self.cursor.execute("INSERT INTO 'tag' ('name', 'desript') VALUES (?, ?)", (name, descript))
-        return self.conn.commit()
+        cursor.execute("INSERT INTO telegram_bot.tag (name, descript) VALUES (%s, %s)", (name, descript))
+        return cnx.commit()
     
-    def write_tag_change(self, name, descript):
+    def get_tag_id(self, name):
+        """Поиск id тэга"""
+        cursor.execute("SELECT tag_id FROM telegram_bot.tag WHERE name = %s", (name,))
+        return cursor.fetchone()
+
+    def write_tag_change(self, tag_id, descript):
         """Изменяем старый тэг"""
-        self.cursor.execute("UPDATE 'tag' SET 'descript' = ? WHERE 'name' = ?", (name, descript))
-        return self.conn.commit()
+        cursor.execute("SET SQL_SAFE_UPDATES = 0")
+        cnx.commit()
+        cursor.execute("UPDATE telegram_bot.tag SET descript = %s WHERE tag_id = %s", (descript, tag_id))
+        return cnx.commit()
 
-    def tag(self, descript):
+    def tag(self, tag_id):
         """Выводим описание тэгов"""
-        result = self.cursor.execute("SELECT 'descript' FROM 'tag' WHERE 'name' = ?", (descript,))
-        return result.fetchone()[0]
+        cursor.execute("SELECT descript FROM telegram_bot.tag WHERE tag_id = %s", (tag_id,))
+        return cursor.fetchone()
 
-    def tag_all(self, name, descript):
+    def tag_all(self):
         """Выводим все тэги"""
-        result = self.cursor.execute("SELECT 'name','descript' FROM 'tag'", (name,descript))
-        return result.fetchone()[0]
+        cursor.execute("SELECT * FROM telegram_bot.tag")
+        return cursor.fetchall()
 
     def close(self):
         """Закрываем соединение с БД"""
--- main.py
+++ main.py
@@ -11,25 +11,101 @@ class BotHandler:
         self.db = BotDB()
         self.token = BOT_TOKEN
         self.api_url = 'https://api.telegram.org/bot5649451560:AAGib_eO3tvCAuqHoNLq5hpya8__69-ZMs4/'
+        self.last_update = 0 
 
 
     def get_updates(self, timeout=30):
         method = 'getUpdates'
-        params = {'timeout': timeout}
+        params = {'offset' : self.last_update + 1,'timeout': timeout}
         resp = requests.get(self.api_url + method, params)
         result_json = resp.json()['result']
-        self.update_id = result_json['update_id']
-        print(self.update_id)
-        for i in result_json:
-            message = i['message']
-            id = str(message['from']['id'])
-            # if message['text'] == '/start':    
-            #     self.start(id)
-            # if message['text'].startswith('/write'):
-            #     text = message['text']
-            #     self.db.create_message(self.db.get_user_id(id), text[7:])
-            #     self.send_message('заметка {self.id} сохранена')
-                
+        if len(result_json) > 0:
+            self.last_update = result_json[-1]['update_id']
+            for i in result_json:
+                message = i['message']
+                #print(message)
+                id = str(message['from']['id'])
+                user_id = self.db.get_user_id(id)[0]
+                telegramUserId = self.db.get_user_id(id)[1]
+                if message['text'] == '/start':    
+                    self.start(id)
+                if message['text'].startswith('/write') and not message['text'].startswith('/write_tag'):
+                    text = message['text'][7:]
+                    create_message, message_id = self.db.create_message(user_id, text)
+                    self.send_message(telegramUserId, f'заметка {message_id} сохранена')
+                if message['text'] == '/read_last':
+                    answer = self.db.read_last(user_id)
+                    self.send_message(telegramUserId, answer)
+                if message['text'].startswith('/read') and message['text'] != '/read_all' and message['text'][:9] != '/read_tag':
+                    message_id = message['text'][6:]
+                    message_id_exist, user_id_db = self.db.message_id_exist(message_id, user_id)
+                    if message_id_exist == False:
+                        if user_id_db == user_id:
+                            self.send_message(telegramUserId, f'заметка {message_id} не найдена')
+                        else:
+                            self.send_message(telegramUserId, f'заметка {message_id} принадлежит другому пользователю')
+                    else:
+                        answer = self.db.read_id(message_id, user_id)
+                        self.send_message(telegramUserId, answer)
+                if message == '/read_all':
+                    answer = self.db.read_all(user_id)
+                    res = ''
+                    for i in answer:
+                        for j in i:
+                            res += j+'\n'
+                    self.send_message(telegramUserId, res)
+                if message['text'][:9] == '/read_tag':
+                    tag = message['text'][10:]
+                    answer = self.db.read_tag(user_id,tag)
+                    res = ''
+                    for i in answer:
+                        for j in i:
+                            res += j+'\n'
+                    self.send_message(telegramUserId, res)
+                if message['text'].startswith('/write_tag'):
+                    write_tag = message['text'].split(' #')[1:]
+                    if len(write_tag) < 2:
+                        continue
+                    tag = '#' + write_tag[0]
+                    descript = '#' + write_tag[1]
+                    tag_id = self.db.get_tag_id(tag)[0]
+                    if self.db.tag_exist(tag) == True:
+                        self.db.write_tag_change(tag_id, descript)
+                    else:
+                        self.db.write_tag_new(tag, descript)
+                if message['text'].startswith('/tag') and message['text'] != '/tag_all':
+                    tags = message['text'].split(' #')[1:]
+                    if len(tags) < 2:
+                        continue
+                    answer = ''
+                    for t in tags:
+                        t = '#' + t
+                        tag_id = self.db.get_tag_id(t)[0]
+                        res = self.db.tag(self.db.get_tag_id(t)[0])[0]
+                        if res == '':
+                            answer += t + ' ' + 'нет описания' + '\n'
+                        else:
+                            answer +=  res + '\n'
+                    self.send_message(telegramUserId, answer)
+                if message['text'] == '/tag_all' and message['text'] != '/tag':
+                    answer = ''
+                    res = self.db.tag_all()
+                    print(answer)
+                    for r in res:
+                        if r[2] != '':
+                            answer += r[2] + '\n'
+                        else:
+                            answer += r[1] + ' нет описания' + '\n'
+                    self.send_message(telegramUserId, answer)
+
+                    
+
+                    
+
+
+
+
+            
 
 
     def start(self,id):
@@ -38,8 +114,8 @@ class BotHandler:
         else:
             self.id = self.db.get_user_id(id)
 
-    def send_message(self, text):
-        params = {'text': text}
+    def send_message(self,id, text):
+        params = {'chat_id':id,'text': text}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
         return resp
